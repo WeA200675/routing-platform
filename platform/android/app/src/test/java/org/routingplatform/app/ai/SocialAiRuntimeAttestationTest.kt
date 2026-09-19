3d88681c12ba2c3dcee9ef4b28de62dc08cc05fd
WeA200675 <78141602+WeA200675@users.noreply.github.com>
2026-09-19 06:21:39 +0200

    G6.20 - Repair attestation fixture newline

--- platform/android/app/src/test/java/org/routingplatform/app/ai/SocialAiRuntimeAttestationTest.kt
+++ platform/android/app/src/test/java/org/routingplatform/app/ai/SocialAiRuntimeAttestationTest.kt
@@ -63,7 +63,8 @@ class SocialAiRuntimeAttestationTest {
         runtimeMetadata: OpenSourceComponentMetadata,
         modelMetadata: LocalModelArtifactMetadata,
     ) = object : LocalSocialAiTextGenerationBackend, SocialAiRuntimeBuildIdentified {
-        override val backendId = "local"\n        override val runtimeBuildId = "build-123"
+        override val backendId = "local"
+        override val runtimeBuildId = "build-123"
         override val runtimeMetadata = runtimeMetadata
         override val modelMetadata = modelMetadata
         override fun generate(request: SocialAiTextGenerationRequest) =
